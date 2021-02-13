0912d51ea02f8866ff74a91117dd0dbe2f7f047d
huntingmarco <laquihonjannel@yahoo.com>
2021-02-13 16:32:41 -0500

    first commit:created simple front,contact page with base html

---
+++ apps/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def frontpage(request):
+    return render(request,'core/frontpage.html')
+
+def contact(request):
+    return render(request,'core/contact.html')
